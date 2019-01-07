14e60b5ac705f891eeb3e51389ac8946f71851ed
kickstarter007 <sam.westfield@hotmail.com>
2019-01-07 14:04:08 -0400

    Adding google maps api key

--- src/config/index.js
+++ src/config/index.js
@@ -4,7 +4,7 @@ let s3PublicContentURL = `https://s3.amazonaws.com/${s3PublicBucket}`;
 export default {
   DEFAULT_ADVENTURE_IMAGE: `${s3PublicContentURL}/HikingLogox2.jpg`,
   FACEBOOK_API_KEY: process.env.REACT_APP_FACEBOOK_API_KEY || '1127821050688033',
-  GOOGLE_MAPS_API_KEY: process.env.REACT_APP_MAPS_API_KEY || '',
+  GOOGLE_MAPS_API_KEY: process.env.REACT_APP_MAPS_API_KEY || 'AIzaSyBC9gh5qpelru4P5qlQ8LkaWZkr6bqFj6M',
   S3_PUBLIC_BUCKET: s3PublicBucket,
   S3_PUBLIC_CONTENT_URL: s3PublicContentURL
 };
